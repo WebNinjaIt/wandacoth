341592d522f83b14a6a91bb026cf3c27c2f69d15
Dmytro Minaiev <dm.minaev03@gmail.com>
2023-12-14 17:58:34 +0200

    updated config

--- wp-config.php
+++ wp-config.php
@@ -25,19 +25,49 @@
  */
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('WP_CACHE', true);
-define( 'WPCACHEHOME', '/home/wandacoth/site/wp-content/plugins/wp-super-cache/' );
-define( 'DB_NAME', 'wandacothdb' );
+
+
+//Using environment variables for DB connection information
+
+// ** Database settings - You can get this info from your web host ** //
+$connectstr_dbhost = getenv('DATABASE_HOST');
+$connectstr_dbname = getenv('DATABASE_NAME');
+$connectstr_dbusername = getenv('DATABASE_USERNAME');
+$connectstr_dbpassword = getenv('DATABASE_PASSWORD');
+
+/** The name of the database for WordPress */
+define('DB_NAME', $connectstr_dbname);
+
 /** MySQL database username */
-define( 'DB_USER', 'wandacothusr' );
+define('DB_USER', $connectstr_dbusername);
+
 /** MySQL database password */
-define( 'DB_PASSWORD', '2d432ac2-dc32-4d28-bfa3-a0166c125ced' );
+define('DB_PASSWORD',$connectstr_dbpassword);
+
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost:6306' );
+define('DB_HOST', $connectstr_dbhost);
+
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', '/home/site/wwwroot/wp-content/plugins/wp-super-cache/' );
+
+// define( 'DB_NAME', 'wandacothdb' );
+/** MySQL database username */
+// define( 'DB_USER', 'wemysqlserver' );
+/** MySQL database password */
+// define( 'DB_PASSWORD', '89080f43-2c20-459d-b6d6-ea92ad115d1a' );
+/** MySQL hostname */
+// define( 'DB_HOST', 'we-mysql-server.mysql.database.azure.com:3306' );
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', 'utf8_unicode_520_ci' );
+
+/** Enabling support for connecting external MYSQL over SSL*/
+$mysql_sslconnect = (getenv('DB_SSL_CONNECTION')) ? getenv('DB_SSL_CONNECTION') : 'true';
+if (strtolower($mysql_sslconnect) != 'false' && !is_numeric(strpos($connectstr_dbhost, "127.0.0.1")) && !is_numeric(strpos(strtolower($connectstr_dbhost), "localhost"))) {
+        define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
+}
+
 /**
  * Authentication Unique Keys and Salts.
  *
@@ -56,6 +86,7 @@ define( 'SECURE_AUTH_SALT',  'VX];# )q7SudM,>{{{i2Q%58q.YW/dtiBz_qX^l62X^y}[=hg8
 define( 'LOGGED_IN_SALT',    ']>!mz~*tFCe32SeVBQ@`]rxLsA,!et9sp`|WA%{Q/(iK)[hf4<sRmO*rGI#%<<@,' );
 define( 'NONCE_SALT',        ')ID}4PTH/>]m)>RNF=PyTsso1PM(7Ae*sbRn~Vad>.SMg$,y)f*Qp.y*#)d1;~U:' );
 define( 'WP_CACHE_KEY_SALT', 'J98v#`DUI^V}e?8rEMMAhPc/o:D}|<}X$H<qsSD1=lR*_!iJCJ W$:V/yA32qRcE' );
+
 /**
  * WordPress Database Table prefix.
  *
@@ -63,10 +94,29 @@ define( 'WP_CACHE_KEY_SALT', 'J98v#`DUI^V}e?8rEMMAhPc/o:D}|<}X$H<qsSD1=lR*_!iJCJ
  * a unique prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix = 'wp_';
-/* That's all, stop editing! Happy publishing. */
+
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy blogging. */
+/**https://developer.wordpress.org/reference/functions/is_ssl/ */
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+        $_SERVER['HTTPS'] = 'on';
+
+$http_protocol='http://';
+if (!preg_match("/^localhost(:[0-9])*/", $_SERVER['HTTP_HOST']) && !preg_match("/^127\.0\.0\.1(:[0-9])*/", $_SERVER['HTTP_HOST'])) {
+        $http_protocol='https://';
+}
+
+//Relative URLs for swapping across app service deployment slots
+define('WP_HOME', $http_protocol . $_SERVER['HTTP_HOST']);
+define('WP_SITEURL', $http_protocol . $_SERVER['HTTP_HOST']);
+define('WP_CONTENT_URL', '/wp-content');
+define('DOMAIN_CURRENT_SITE', $_SERVER['HTTP_HOST']);
+
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+        define( 'ABSPATH', __DIR__ . '/' );
 }
+
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
